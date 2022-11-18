17abeb29f2d487bdb0f4ed619e9f4fe3718d5fcc
hyhoop <leoestudios@outlook.es>
2022-11-18 21:09:52 +0100

    Error solucionado

--- snake.py
+++ snake.py
@@ -13,10 +13,11 @@ my_positon = [0, 0]
 end_game = False
 map_objects = []
 tail = []
+score = 0
 
 while end_game == False:
     while len(map_objects) != NUM_OF_OBJECTS:
-        new_object = [randint(0, MAP_WIDTH - 1), randint(0, MAP_HEIGHT)]
+        new_object = [randint(0, MAP_WIDTH - 1), randint(0, MAP_HEIGHT - 1)]
 
         if new_object not in map_objects and new_object != my_positon:
             map_objects.append(new_object)
@@ -35,7 +36,7 @@ while end_game == False:
                     tail_in_cell = tail_piece
 
             for objects in map_objects:
-                if objects[POS_X] == cordinate_x and objects[POS_Y] == cordinate_y:
+                if objects[POS_X] == cordinate_x and objects[POS_Y] == cordinate_y and != my_position:
                     char_to_set = "*"
                     object_in_cell = objects
 
@@ -44,6 +45,7 @@ while end_game == False:
                 if object_in_cell:
                     map_objects.remove(object_in_cell)
                     TAIL_LENGTH += 1
+                    score += 1
                 if tail_in_cell:
                     end_game = True
 
@@ -56,26 +58,29 @@ while end_game == False:
     direction = readchar.readchar()
 
     if direction == "w":
-        my_positon[POS_Y] -= 1
-        tail.insert(1, my_positon.copy())
-        my_positon[POS_Y] %= MAP_HEIGHT
+        tail.insert(0, my_positon.copy())
         tail = tail[:TAIL_LENGTH]
+        my_positon[POS_Y] -= 1
+        my_positon[POS_Y] %= MAP_HEIGHT
     elif direction == "a":
+        tail.insert(0, my_positon.copy())
+        tail = tail[:TAIL_LENGTH]
         my_positon[POS_X] -= 1
         my_positon[POS_X] %= MAP_WIDTH
-        tail.insert(1, my_positon.copy())
-        tail = tail[:TAIL_LENGTH]
     elif direction == "s":
+        tail.insert(0, my_positon.copy())
+        tail = tail[:TAIL_LENGTH]
         my_positon[POS_Y] += 1
         my_positon[POS_Y] %= MAP_HEIGHT
-        tail.insert(1, my_positon.copy())
-        tail = tail[:TAIL_LENGTH]
     elif direction == "d":
+        tail.insert(0, my_positon.copy())
+        tail = tail[:TAIL_LENGTH]
         my_positon[POS_X] += 1
         my_positon[POS_X] %= MAP_WIDTH
-        tail.insert(1, my_positon.copy())
-        tail = tail[:TAIL_LENGTH]
     elif direction == "q":
         end_game = True
 
     os.system("cls")
+    print("score: {}".format(score))
+
+print("GAME OVER WITH: {}".format(score))
